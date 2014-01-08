b3e7c12873803d3c603c05701960e53bae836800
marechalc <christophe.marechal@edu.ge.ch>
2014-01-08 11:31:15 +0100

    initial revision

---
+++ sandbox/helloGit.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git");
+        }
+    }
+}
