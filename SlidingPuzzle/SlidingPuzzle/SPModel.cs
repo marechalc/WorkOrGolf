1039e288710308f73a601bb81fd1ae84280ddec5
peiryj <jordan.peiry@gmail.com>
2014-01-22 11:17:41 +0100

    Commit Controller & Model

---
+++ SlidingPuzzle/SlidingPuzzle/SPModel.cs
@@ -0,0 +1,20 @@
+﻿/************
+ * Authors : M. Pereira & J. Peiry
+ * Date    : 22.01.14
+ * Desc    : Model for sliding puzzle
+ * **********/
+
+using System;
+namespace SlidingPuzzle
+{
+    class SPModel
+    {
+        /// <summary>
+        /// Constructor of the model
+        /// </summary>
+        public SPModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
