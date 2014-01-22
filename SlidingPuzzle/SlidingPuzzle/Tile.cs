3f878224496cf74e358b25bedbc234b9c7240a49
Loïc Damien <dzamlo@example.com>
2014-01-22 11:01:02 +0100

    Implementation of the Tile class

--- SlidingPuzzle/SlidingPuzzle/Tile.cs
+++ SlidingPuzzle/SlidingPuzzle/Tile.cs
@@ -5,7 +5,42 @@ using System.Text;
 
 namespace SlidingPuzzle
 {
-    class Tile
+    public class Tile
     {
+        #region fields
+        bool _isAllowed;
+        int _symbolId;
+        #endregion
+
+        #region properties
+        public int SymbolId
+        {
+            get { return _symbolId; }
+            set { _symbolId = value; }
+        }
+
+        public bool IsAllowed
+        {
+            get { return _isAllowed; }
+            set { _isAllowed = value; }
+        }
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tile"/> class.
+        /// </summary>
+        public Tile():this(true,0)
+        { 
+            //NO CODE
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tile"/> class.
+        /// </summary>
+        public Tile(bool isAllowed, int symbolId)
+        {
+            IsAllowed = isAllowed;
+            SymbolId = symbolId;
+        }
     }
 }
