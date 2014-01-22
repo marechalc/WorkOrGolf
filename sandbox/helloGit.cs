4513d91b6d57a90eafec657a5c82b78e5bbe56ea
Christopher Rejas <rejas.c@gmail.com>
2014-01-22 10:11:56 +0100

    Hello Rejas

--- sandbox/helloGit.cs
+++ sandbox/helloGit.cs
@@ -9,13 +9,14 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
-	    Console.WriteLine("Hello Menetrey");
+			Console.WriteLine("Hello Menetrey");
             Console.WriteLine("Hello Zanos");
             Console.WriteLine("Hello Sampaio");
             Console.WriteLine("Hello San Peiry");
             Console.WriteLine("Hello Damien");
             Console.WriteLine("Hello Lopes");
 			Console.WriteLine("Hello Pereira");
+			Console.WriteLine("Hello Rejas");
         }
     }
 }
