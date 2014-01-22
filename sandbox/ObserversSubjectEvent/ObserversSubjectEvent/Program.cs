1f583584b3505920d6eea32b0bf34b3364e814e1
Christopher Rejas <rejas.c@gmail.com>
2014-01-22 09:01:32 +0100

    Test

--- sandbox/ObserversSubjectEvent/ObserversSubjectEvent/Program.cs
+++ sandbox/ObserversSubjectEvent/ObserversSubjectEvent/Program.cs
@@ -12,20 +12,25 @@ namespace ObserversSubjectEvent
         static void Main(string[] args)
         {
             Subject subject = new Subject();
+
             Observer observer1 = new Observer(1);
             Console.WriteLine("Subscribe observer 1.");
             subject.OnRateChanged += observer1.Notify;
+
             Observer observer2 = new Observer(2);
             Console.WriteLine("Subscribe observer 2.");
             subject.OnRateChanged += observer2.Notify;
+
             Observer observer3 = new Observer(3);
             Console.WriteLine("Subscribe observer 3.");
             subject.OnRateChanged += observer2.Notify;
             Observer observer666 = new Observer(666); //le diable
             Console.WriteLine("Subscribe observer 666.");
             subject.OnRateChanged += observer3.Notify;
+
             Console.WriteLine("\nChange the rate field of the subject.");
             subject.Rate += .5;
+
             Console.WriteLine("\nUnsubscribe observer1.");
             Console.WriteLine("\nChange the rate field of the subject.");
             subject.OnRateChanged -= observer1.Notify;
