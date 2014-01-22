72baa62576523d6c9473c6f87ba48bcdb90c8f49
Premtim Bajrami <premtimbajrami@gmail.com>
2014-01-22 10:56:37 +0100

    Piece class creation

---
+++ SlidingPuzzle/SlidingPuzzle/Piece.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Title : Piece Class
+ * Version :
+ * 1.0 : Creation - BAJRAMI & TUX
+ */
+using System.Drawing;
+
+namespace SlidingPuzzle
+{
+  class Piece
+  {
+    #region Fields
+    private Rectangle _rect;
+    private int _symbolId;
+    #endregion
+
+    #region Properties
+    public int SymbolId
+    {
+      get { return _symbolId; }
+      set { _symbolId = value; }
+    }
+
+    public Rectangle Rect
+    {
+      get { return _rect; }
+      set { _rect = value; }
+    }
+    #endregion
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Piece()
+      : this(new Rectangle(), 0)
+    {
+      // No code
+    }
+
+    /// <summary>
+    /// Designated constructor
+    /// </summary>
+    /// <param name="rect">Size and position of the piece</param>
+    /// <param name="symbolId">Index of the symbol</param>
+    public Piece(Rectangle rect, int symbolId)
+    {
+      this.Rect = rect;
+      this.SymbolId = symbolId;
+    }
+  }
+}
