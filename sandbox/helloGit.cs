880badc27ec666f70d7184d28bb28bee197d4066
peiryj <jordan.peiry@gmail.com>
2014-01-22 09:15:42 +0100

    Added peiry

--- sandbox/helloGit.cs
+++ sandbox/helloGit.cs
@@ -12,6 +12,7 @@ namespace HelloGit
 			Console.WriteLine("Hello Menetrey");
 			Console.WriteLine("Hello Zanos");
             Console.WriteLine("Hello Sampaio");
+			Console.WriteLine("Hello Mr. Peiry");
 			Console.WriteLine("Hello parking meter !");
         }
     }
