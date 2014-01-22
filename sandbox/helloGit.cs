deb914872398dd7b2f92e02ae1ac03f18c161cc1
Zanos <alekoskakos@hotmail.com>
2014-01-22 09:41:36 +0100

    Update helloGit.cs

--- sandbox/helloGit.cs
+++ sandbox/helloGit.cs
@@ -9,7 +9,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
-	        Console.WriteLine("Hello Menetrey");
+	    Console.WriteLine("Hello Menetrey");
             Console.WriteLine("Hello Zanos");
             Console.WriteLine("Hello Sampaio");
             Console.WriteLine("Hello Mr. Peiry");
