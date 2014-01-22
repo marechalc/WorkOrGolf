fe16eafd064b394e1e4e9918c1092403c408f3ae
Miguelito993 <miguel.pereira.vieira@gmail.com>
2014-01-22 10:18:35 +0100

    Correct a mistake

--- sandbox/helloGit.cs
+++ sandbox/helloGit.cs
@@ -12,7 +12,7 @@ namespace HelloGit
             Console.WriteLine("Hello Menetrey");
             Console.WriteLine("Hello Zanos");
             Console.WriteLine("Hello Sampaio");
-            Console.WriteLine("Hello San Peiry");
+            Console.WriteLine("Hello Peiry San");
             Console.WriteLine("Hello Damien");
             Console.WriteLine("Hello Lopes");
             Console.WriteLine("Hello Pereira");
