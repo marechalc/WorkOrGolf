a9f296a61779db56454cec994bab0a54d90b4f6f
Loïc Damien <dzamlo@example.com>
2014-02-05 10:59:05 +0100

    Added constant and correct indentation

--- SlidingPuzzle/SlidingPuzzle/Piece.cs
+++ SlidingPuzzle/SlidingPuzzle/Piece.cs
@@ -7,45 +7,51 @@ using System.Drawing;
 
 namespace SlidingPuzzle
 {
-  public class Piece
-  {
-    #region Fields
-    private Rectangle _rect;
-    private int _symbolId;
-    #endregion
-
-    #region Properties
-    public int SymbolId
+    public class Piece
     {
-      get { return _symbolId; }
-      set { _symbolId = value; }
-    }
+        #region Consts
+        static readonly Point DEFAULT_LOCATION = new Point(0,0);
+        static readonly Size DEFAULT_SIZE = new Size(1, 1);
+        const int DEFAULT_SYMBOL_ID = 0;
+        #endregion
 
-    public Rectangle Rect
-    {
-      get { return _rect; }
-      set { _rect = value; }
-    }
-    #endregion
+        #region Fields
+        private Rectangle _rect;
+        private int _symbolId;
+        #endregion
 
-    /// <summary>
-    ///  Initializes a new instance of the <see cref="SlidingPuzzle"/> class.
-    /// </summary>
-    public Piece()
-      : this(new Rectangle(new Point(0,0), new Size(1,1)), 0)
-    {
-      // No code
-    }
+        #region Properties
+        public int SymbolId
+        {
+            get { return _symbolId; }
+            set { _symbolId = value; }
+        }
 
-    /// <summary>
-    ///  Initializes a new instance of the <see cref="SlidingPuzzle"/> class with a rectangle and a symbol id.
-    /// </summary>
-    /// <param name="rect">Size and position of the piece</param>
-    /// <param name="symbolId">Index of the symbol</param>
-    public Piece(Rectangle rect, int symbolId)
-    {
-      this.Rect = rect;
-      this.SymbolId = symbolId;
+        public Rectangle Rect
+        {
+            get { return _rect; }
+            set { _rect = value; }
+        }
+        #endregion
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="SlidingPuzzle"/> class.
+        /// </summary>
+        public Piece()
+            : this(new Rectangle(DEFAULT_LOCATION, DEFAULT_SIZE), DEFAULT_SYMBOL_ID)
+        {
+            // No code
+        }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="SlidingPuzzle"/> class with a rectangle and a symbol id.
+        /// </summary>
+        /// <param name="rect">Size and position of the piece</param>
+        /// <param name="symbolId">Index of the symbol</param>
+        public Piece(Rectangle rect, int symbolId)
+        {
+            this.Rect = rect;
+            this.SymbolId = symbolId;
+        }
     }
-  }
 }
