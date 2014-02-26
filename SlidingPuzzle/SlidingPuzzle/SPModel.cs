9c452f2692531d551c7127f02b91f3885f144b02
ZanosA <alekoskakos@hotmail.com>
2014-02-26 09:34:13 +0100

    add classes as references

--- SlidingPuzzle/SlidingPuzzle/SPModel.cs
+++ SlidingPuzzle/SlidingPuzzle/SPModel.cs
@@ -1,22 +1,31 @@
-﻿/****************************************
- * Authors : M. Pereira & J. Peiry
- * Date    : 22.01.14
- * Desc    : Model for sliding puzzle
- * **************************************/
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlidingPuzzle;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
-using System;
 namespace SlidingPuzzle
 {
-    class SPModel {
+    [Serializable]
+    class SPModel
+    {
+        const string DEFAULT_SAVE_FILENAME = "MyGame";
 
         #region fields & get/set
         private Game _game;
 
-        internal Game Game {
-            get {
+        internal Game Game
+        {
+            get
+            {
                 if (_game == null)
-                    _game = new Game(); //lazy initialization                
-                return _game; }
+                    _game = new Game(); // lazy initialization
+                return _game;
+                // return (_game == null) ? new Game() : _game;
+            }
             set { _game = value; }
         }
         #endregion
@@ -26,7 +35,25 @@ namespace SlidingPuzzle
         /// </summary>
         public SPModel()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException(); // The devil is hiding in the details.
+        }
+
+        public void Serialize(string filename)
+        {
+            // Reference : http://msdn.microsoft.com/.../lib.../4abbf6k0(v=vs.110).aspx
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, this.Game);
+            stream.Close();
+        }
+
+        public void Unserialize(string filename)
+        {
+            // Reference : http://msdn.microsoft.com/.../lib.../4abbf6k0(v=vs.110).aspx
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            this.Game = (Game)formatter.Deserialize(stream);
+            stream.Close();
         }
     }
 }
